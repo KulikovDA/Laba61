6fd59c8c0b27bc55ca226ffe4af9719938e0f376
KulikovDA <144238632+KulikovDA@users.noreply.github.com>
2023-12-10 19:07:38 +0200

    Program.cs

--- Laba61/Program.cs
+++ Laba61/Program.cs
@@ -1,26 +1,18 @@
-﻿class Program
+//базовый 3
+class Program
 {
-    static int SumOfDigits(int number)
-    {
-        int sum = 0;
-        while (number > 0)
-        {
-            sum += number % 10;
-            number = number / 10;
-        }
-        return sum;
-    }
-
     static void Main()
     {
-        for (int i = 1000; i <= 9999; i++)
-        {
-            int firstTwoDigits = i / 100;
-            int lastTwoDigits = i % 100;
-            if (SumOfDigits(firstTwoDigits) == SumOfDigits(lastTwoDigits))
-            {
-                Console.WriteLine(i);
-            }
-        }
+        Console.Write("Введите значение a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение b: ");
+        int b = int.Parse(Console.ReadLine());
+        int z = Max(a, 2 * b) + Max(2 * a - b, b);
+        Console.WriteLine("Значение z = " + z);
+    }
+
+    static int Max(int x, int y)
+    {
+               return x > y ? x : y;
     }
 }
