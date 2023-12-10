6b43a5ec1cd37a766754870f3000aefba03e12f3
Arion <Arion@DESKTOP-9BU80RS>
2023-12-10 15:33:25 +0200

    Добавьте файлы проекта.

---
+++ Laba61/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static int SumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        for (int i = 1000; i <= 9999; i++)
+        {
+            int firstTwoDigits = i / 100;
+            int lastTwoDigits = i % 100;
+            if (SumOfDigits(firstTwoDigits) == SumOfDigits(lastTwoDigits))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
